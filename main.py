90ef1cefbcf717c82903d18734244f7e8ce27d50
prostoLavr <prostoLawr@gmail.com>
2022-03-12 13:56:21 +0800

    Off file size limit

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ TEST = False
 UPLOAD_FOLDER = 'static/files'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAX_CONTENT_LENGTH'] = 256 * 1024 * 1024
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'hello'
 
