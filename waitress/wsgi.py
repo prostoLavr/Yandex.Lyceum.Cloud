862699b89e95d2c564283e40074f78030ae82895
lawrence <prostoLawr@gmail.com>
2022-05-03 16:04:35 +0800

    Add test run

--- waitress/wsgi.py
+++ waitress/wsgi.py
@@ -3,4 +3,8 @@ from waitress import serve
 
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=5000)
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] in ('--test', 'test', '-t'):
+        app.run(host='0.0.0.0', port=3456, debug=True)
+    else:
+        serve(app, host='0.0.0.0', port=5000)
