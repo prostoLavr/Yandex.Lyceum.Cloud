572a13fea8b960d622d3467eb56c6a2ae904e80e
lawrence <prostoLawr@gmail.com>
2022-05-10 23:53:31 +0800

    Add link copy button

---
+++ python/app/static/js/text_copy.js
@@ -0,0 +1,13 @@
+function copyText(link) {
+    const el = document.createElement("textarea");
+    el.value = link;
+    el.setAttribute("readonly", "");
+    el.style.position = "absolute";
+    el.style.left = "-9999px";
+    document.body.appendChild(el);
+    el.select();
+    el.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    document.getElementById("linkButton").textContent = "Link copied!"
+}
