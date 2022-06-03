3c858383269f20ebcf9664c01c5fefdefc4228dd
Demyan228 <bubnovdema@gmail.com>
2022-06-03 20:53:08 +0800

    fix footer

--- app/smart_views.py
+++ app/smart_views.py
@@ -144,7 +144,7 @@ def messenger():
     mes = None
     if request.method == 'POST':
         friend_name = request.form.get('Friend_Login')
-        db_manager.add_friend(current_user, friend_name)
+        mes = db_manager.add_friend(current_user, friend_name)
     user_friends = db_manager.get_friends_for_user(current_user)
     user_requests = db_manager.get_friend_requests(current_user)
     return my_render_template('messenger.html', users=user_friends,
